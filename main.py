3544c104f690e4be2bab93ee5379b332b5f33e11
yu_ea <saki4017121@gmail.com>
2026-06-14 17:46:47 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+try:
+    score = int(input('成績評價:'))
+    if score < 0 or score >100:
+         print('成績錯誤!重新輸入')
+    elif score < 59:
+          print('不及格')
+    elif score < 75:
+          print('及格')
+    elif score < 85:
+          print('良好')
+    else:
+          print('優秀')
+except ValueError:
+        print('成績錯誤!重新輸入')
+
+print('感謝測試')
